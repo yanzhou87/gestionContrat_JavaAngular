f44e87396a540af4d40cfdd88c9f1baa5b2653c3
Yan Zhou <zhouyan87910@gmail.com>
2022-10-27 10:18:33 -0400

    first commit

---
+++ src/main/java/com/example/projet2contrats/Projet2ContratsApplication.java
@@ -0,0 +1,13 @@
+package com.example.projet2contrats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet2ContratsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projet2ContratsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projet2contrats/Projet2ContratsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet2contrats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projet2ContratsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
